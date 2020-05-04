bb505d70e0166189cc0de32616556f07ee31b1c3
CookieBilly <41408185+CookieBilly@users.noreply.github.com>
2020-05-04 23:12:51 +0100

    Update ready.js

--- events/discord/ready.js
+++ events/discord/ready.js
@@ -1,10 +1,6 @@
-
-const waitingList = require('../../modules/manageGuild.js');
-const minecraft = require('../../modules/minecraftBot');
-const db = require('../../modules/database');
 const config = require('../../settings/config.json');
 
 module.exports = async (client) => {
 	console.log(`[D] - Logged in as ${client.user.tag}`);
-  client.user.setActivity(`Loud Music`);
+	client.user.setActivity(`Loud Music`);
 };
