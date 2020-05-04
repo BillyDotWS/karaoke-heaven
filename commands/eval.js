f5703df7ef3c8db425fcbcdf1cacf05b100a11cc
CookieBilly <41408185+CookieBilly@users.noreply.github.com>
2020-05-04 22:15:39 +0100

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -2,14 +2,10 @@ module.exports = {
 	name: 'eval',
 	description: 'Run a piece of code from Discord',
 	usage: '-eval <code>',
-	requiredRoles: ['Guild Master'],
-	allowedUsers: ['686643076420206597', '213849560508792832'],
+	requiredRoles: ['☕ Developer'],
+	allowedUsers: ['281199286765748225', '213849560508792832'],
 	argsNeeded: 1,
 	async execute(client, message, args) {
-		if (filteredCode.some(w => args.join(' ').toLowerCase().includes(w))) {
-			message.channel.send(`▶️ **Input**\n\`\`\`${args.join(' ')}\`\`\`\n\n◀️ **Output**\n\`\`\`Eval Block, don't test me bitch\`\`\``);
-			return;
-		}
 		try {
 			var evaledCode = require('util').inspect(eval(args.join(' '), {
 				depth: 0,
