6d36eeeb749c68c6baa16c270757d72ed176140e
CookieBilly <41408185+CookieBilly@users.noreply.github.com>
2020-05-04 22:09:35 +0100

    Update message.js

--- events/discord/message.js
+++ events/discord/message.js
@@ -1,7 +1,5 @@
 const Discord = require('discord.js');
 const fs = require('fs');
-const verify = require('../../modules/verifyUser');
-const guildBridge = require('../../modules/guildBridge');
 const embeds = require('../../modules/embeds');
 const config = require('../../settings/config.json');
 
@@ -17,10 +15,30 @@ module.exports = async (client, message) => {
 		client.commands.set(command.name, command);
 	}
 
+	if (message.channel.name === 'guild-bridge' || message.channel.name === 'officer-bridge') {
+		if (message.author.bot) return;
+		else return guildBridge.bridgeToMC(message, message.channel.name);
+	}
+
 	const messageAsArray = message.content.split(/ +/);
 	const args = messageAsArray.slice(1);
+
+	if (message.channel.name === 'verification') {
+		if (message.content.split(' ').length === 1) {
+			message.delete();
+			const discordUser = client.users.cache.get(message.author.id);
+			return verify.linkAccount(message, false, discordUser, message.content.split(' ')[0], null);
+		}
+		else {return message.delete();}
+	}
 
 	const command = client.commands.get(messageAsArray[0].slice(config.prefix.length).toLowerCase());
 
 	if (!message.content.startsWith(config.prefix) || !command) return;
+
+	if (message.member.roles.cache.some(r => command.requiredRoles.includes(r.name)) || command.allowedUsers.includes(message.author.id)) {
+		if (command.argsNeeded <= args.length) command.execute(client, message, args);
+		else new embeds.infoEmbed('error', message.channel, `That command requires at least **${command.argsNeeded - args.length}** arg(s) to run`, message.author, 10).sendEmbed();
+	}
+	else {new embeds.infoEmbedembeds.sendEmbed('error', message.channel, `You must have the role ${message.guild.roles.cache.find(role => role.name === command.requiredRoles[0])} to run that command`, message.author, 10).sendEmbed();}
 };
