b9c96172e8bf633b879ad802daa429c3e069aeb1
CookieBilly <41408185+CookieBilly@users.noreply.github.com>
2020-05-04 23:16:30 +0100

    Update ready.js

--- events/discord/ready.js
+++ events/discord/ready.js
@@ -1,6 +1,6 @@
 const config = require('../../settings/config.json');
 
 module.exports = async (client) => {
-	console.log(`[D] - Logged in as ${client.user.tag}`);
+	console.log(`[/events/discord/ready.js] Logged in as ${client.user.tag}`);
 	client.user.setActivity(`Loud Music`);
 };
