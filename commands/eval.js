0477dff8d14bf8708dbd55af8f31b91c92ab446e
CookieBilly <41408185+CookieBilly@users.noreply.github.com>
2020-05-05 15:31:35 +0100

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -8,48 +8,38 @@ module.exports = {
   requiredRoles: ['☕ Developer'],
   allowedUsers: ['707010477917470761', '213849560508792832'],
   argsNeeded: 1,
-  async execute(client, message, args) {
-    try {
-      var evaledCode = require('util').inspect(eval(args.join(' '), {
-        depth: 0,
-      }));
+	async execute(client, message, args) {
+		if (filteredCode.some(w => args.join(' ').toLowerCase().includes(w))) {
+			message.channel.send(`▶️ **Input**\n\`\`\`${args.join(' ')}\`\`\`\n\n◀️ **Output**\n\`\`\`Eval Block, don't test me bitch\`\`\``);
+			return;
+		}
+		try {
+			var evaledCode = require('util').inspect(eval(args.join(' '), {
+				depth: 0,
+			}));
 
-      if (typeof evaledCode !== 'string') {
-        evaledCode = require('util').inspect(evaledCode);
-      }
+			if (typeof evaledCode !== 'string') {
+				evaledCode = require('util').inspect(evaledCode);
+			}
 
-      var evalEmbed = new Discord.MessageEmbed()
-      evalEmbed.setTitle('Eval Result')
-      evalEmbed.setDescription(`:white_check_mark: **Success!**`)
-      evalEmbed.addField('Output', `${evaledcode}`, false)
-      evalEmbed.setAuthor(message.author.username, message.author.displayAvatarURL({ format: "png"}))
-      evalEmbed.setColor('#FFC0CB')
-      evalEmbed.setFooter('Karaoke Heaven Bot', 'https://i.imgur.com/2w4KKRE.png')
-      message.channel.send(evalEmbed).catch(() => {
-        return message.channel.send({
-          files: [{
-            attachment: Buffer.from(evaledCode),
-            name: 'output.txt',
-          }],
-        });
-      });
-    }
-    catch (e) {
-      var evalEmbed = new Discord.MessageEmbed()
-      evalEmbed.setTitle('Eval Result')
-      evalEmbed.setDescription(`:warning: **Error!**`)
-      evalEmbed.addField('Output', `${e.stack}`, false)
-      evalEmbed.setAuthor(message.author.username, message.author.displayAvatarURL({ format: "png"}))
-      evalEmbed.setColor('#FFC0CB')
-      evalEmbed.setFooter('Karaoke Heaven Bot', 'https://i.imgur.com/2w4KKRE.png')
-      message.channel.send(evalEmbed).catch(() => {
-        return message.channel.send({
-          files: [{
-            attachment: Buffer.from(evaledCode),
-            name: 'error.txt',
-          }],
-        });
-      });
-    }
-  },
+			message.channel.send(`${message.author}\n▶️ **Input**\n\`\`\`${args.join(' ')}\`\`\`\n\n◀️ **Output**\n\`\`\`${evaledCode}\`\`\``).catch(() => {
+				return message.channel.send({
+					files: [{
+						attachment: Buffer.from(evaledCode),
+						name: 'output.txt',
+					}],
+				});
+			});
+		}
+		catch (e) {
+			message.channel.send(`${message.author}\n▶️ **Input**\n\`\`\`${args.join(' ')}\`\`\`\n\n◀️ **Output**\n\`\`\`${e.stack}\`\`\``).catch(() => {
+				return message.channel.send({
+					files: [{
+						attachment: Buffer.from(evaledCode),
+						name: 'error.txt',
+					}],
+				});
+			});
+		}
+	},
 };
