598ce7e5bc57dbb1e4d8ad6d6e1eeda73a51f8f4
CookieBilly <41408185+CookieBilly@users.noreply.github.com>
2020-05-05 03:47:01 +0100

    Update modping.js

--- commands/modping.js
+++ commands/modping.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'modping',
 	description: 'Ping moderators about an issue/question',
 	usage: 'modping <reason>',
-	requiredRoles: ['👪 Member'],
+	requiredRoles: ['@everyone'],
 	allowedUsers: ['707010477917470761', '213849560508792832'],
 	argsNeeded: 1,
 	async execute(client, message, args) {
