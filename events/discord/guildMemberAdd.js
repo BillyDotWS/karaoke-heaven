b11b9d5b0dff115a305e2e955bdee587231cb562
CookieBilly <41408185+CookieBilly@users.noreply.github.com>
2020-05-05 18:05:41 +0100

    Update guildMemberAdd.js

--- events/discord/guildMemberAdd.js
+++ events/discord/guildMemberAdd.js
@@ -1,3 +1,12 @@
+const Discord = require('discord.js')
+
 module.exports = async (client, member) => {
 	member.roles.add(['700213602098872350']);
+	
+	var welcomeEmbed = new Discord.MessageEmbed()
+	welcomeEmbed.setTitle(':wave: **Welcome to Karaoke Heaven!**')
+	welcomeEmbed.setColor('#00FF00')
+        welcomeEmbed.setDescription(`This discord is the home to many exciting events. You're even welcome to host your own events.`)
+        member.send(welcomeEmbed)
+				
 };
