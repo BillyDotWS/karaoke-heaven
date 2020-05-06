c376511de9989b0764eb41a1edd6bf06bd896e98
CookieBilly <41408185+CookieBilly@users.noreply.github.com>
2020-05-06 03:15:59 +0100

    Update guildMemberUpdate.js

--- events/discord/guildMemberUpdate.js
+++ events/discord/guildMemberUpdate.js
@@ -8,6 +8,10 @@ module.exports = async (client, oldMember, newMember) => {
 
 	// Detect if someone recieves the bouncer role
 	if (!oldRoles.includes(config.roles.bouncer) && newRoles.includes(config.roles.bouncer)) {
-		client.channels.cache.get('706303003400994957').send(`OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG GUESS WHAT? **${newMember.user.tag}** BOUNCER @everyone PANIC!`)
+		var bouncerEmbed = new Discord.MessageEmbed()
+		bouncerEmbed.setTitle('🧍🏿‍♂️ **Congratulations!**')
+		bouncerEmbed.setDescription(`You have been added to the \`🧍🏿‍♂️  Bouncer\` role in the Karaoke Heaven server! We can't wait to get to know you and welcome you to our team. In order to get you fully setup, we'll have to get you into our slack!\n\n:speech_balloon: **Joining Slack:**\nIn order to download slack, [click here](https://slack.com/intl/en-gb/downloads/windows). Once you have downloaded slack, check your email account: you will have an invite to our slack workspace, please do not share any information about this.\n\n**:confused: Need help?**\n\nFeel free to private message a Manager with any questions you have about joining our slack workspacce or any other questions you have. We'll be happy to help you.`)
+		bouncerEmbed.setColor('#00FF00')
+                newMember.send(bouncerEmbed)
 	}
 };
