13771571b2e436287843387304b716826df1ba01
CookieBilly <41408185+CookieBilly@users.noreply.github.com>
2020-05-06 01:52:21 +0100

    Create info.js

---
+++ commands/info.js
@@ -0,0 +1,7 @@
+• Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
+• Uptime     :: ${duration}
+• Users      :: ${client.users.size.toLocaleString()}
+• Servers    :: ${client.guilds.size.toLocaleString()}
+• Channels   :: ${client.channels.size.toLocaleString()}
+• Discord.js :: v${version}
+• Node       :: ${process.version}`, {code: "asciidoc"});
