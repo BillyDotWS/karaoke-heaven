6c12361c4b28ac6b60126bd70129deebd87b1a0c
CookieBilly <41408185+CookieBilly@users.noreply.github.com>
2020-05-06 01:59:48 +0100

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -1,7 +1,27 @@
-• Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
-• Uptime     :: ${duration}
-• Users      :: ${client.users.size.toLocaleString()}
-• Servers    :: ${client.guilds.size.toLocaleString()}
-• Channels   :: ${client.channels.size.toLocaleString()}
-• Discord.js :: v${version}
-• Node       :: ${process.version}`, {code: "asciidoc"});
+const Discord = require('discord.js')
+const config = require('../settings/config.json')
+const { version } = require("discord.js");
+const moment = require("moment");
+require("moment-duration-format");
+
+module.exports = {
+    name: 'info',
+    description: 'View information about the bot',
+    usage: 'info',
+    requiredRoles: ['👪 Member'],
+    allowedUsers: [],
+    argsNeeded: 0,
+    async execute(client, message, args, embeds) {
+        const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
+        var commandembed = new Discord.MessageEmbed()
+        commandembed.setTitle('Bot Information')
+        commandembed.addField('Mem Usage:', `\`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB\``, true)
+        commandembed.addField('Uptime:', `\`${duration}\``, true)
+        commandembed.addField('Users:', `\`${client.users.size.toLocaleString()}\``, true)
+        commandembed.addField('Discord.js:', `\`v${version}\``, true)
+        commandembed.addField('Discord.js:', `\`v${version}\``, true)
+        commandembed.addField('Node:', `\`${process.version}\``, true)
+        message.channel.send(commandembed)
+    {
+
+}
