d77e8fce60e4ad61e0dd4f0871b9b9ae352f7782
CookieBilly <41408185+CookieBilly@users.noreply.github.com>
2020-05-06 23:48:53 +0100

    Update modping.js

--- commands/modping.js
+++ commands/modping.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'modping',
 	description: 'Ping moderators about an issue/question',
 	usage: 'modping <reason>',
-	exammple: 'modping People are spamming',
+	example: 'modping People are spamming',
 	requiredRoles: ['@everyone'],
 	allowedUsers: ['707010477917470761', '213849560508792832'],
 	argsNeeded: 1,
