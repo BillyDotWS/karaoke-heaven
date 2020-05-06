1e2e9060b0b35dfb276db2ff038cf0ee0e5d193c
Mathew <mat.sinadinos@gmail.com>
2020-05-05 20:57:50 -0500

    feat(roles): detect when a user is given bouncer role

--- events/discord/guildMemberUpdate.js
+++ events/discord/guildMemberUpdate.js
@@ -1,62 +1,13 @@
 module.exports = async (client, oldMember, newMember) => {
+	// Config File
+	const config = require('../../settings/config.json');
 
-	// declare changes
-	var Changes = {
-	  unknown: 0,
-	  addedRole: 1,
-	  removedRole: 2,
-	  username: 3,
-	  nickname: 4,
-	  avatar: 5
+	// Map old and new roles
+	const oldRoles = oldMember.roles.cache.map(g => g.id);
+	const newRoles = newMember.roles.cache.map(g => g.id);
+
+	// Detect if someone recieves the bouncer role
+	if (!oldRoles.includes(config.roles.bouncer) && newRoles.includes(config.roles.bouncer)) {
+		client.channels.cache.get('706303003400994957').send(`OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG GUESS WHAT? **${newMember.tag}** BOUNCER @everyone PANIC!`)
 	}
-	var change = Changes.unknown
-  
-	// check if roles were removed
-	var removedRole = ''
-	oldMember.roles.every(function (value) {
-	  if (newMember.roles.find('id', value.id) == null) {
-		change = Changes.removedRole
-		removedRole = value.name
-	  }
-	})
-  
-	// check if roles were added
-	var addedRole = ''
-	newMember.roles.every(function (value) {
-	  if (oldMember.roles.find('id', value.id) == null) {
-		change = Changes.addedRole
-		addedRole = value.name
-	  }
-	})
-  
-	// check if username changed
-	if (newMember.user.username != oldMember.user.username) {
-	  change = Changes.username
-	}
-	// check if nickname changed
-	if (newMember.nickname != oldMember.nickname) {
-	  change = Changes.nickname
-	}
-	// check if avatar changed
-	if (newMember.user.avatarURL != oldMember.user.avatarURL) {
-	  change = Changes.avatar
-	  
-	}
-    switch (change) {
-		case Changes.unknown:
-		  break
-		case Changes.addedRole:
-		  newMember.send(`You were added to the \`${addedRole}\` role!`)
-		  break
-		case Changes.removedRole:
-			newMember.send(`You were removed from the \`${removedRole}\` role!`)
-			break
-		case Changes.username:
-		  break
-		case Changes.nickname:
-		  break
-		case Changes.avatar:
-		  break
-	}
-	
-  };
+};
