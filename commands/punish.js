96e119feb749235d77f13cef9618865a39c809f8
CookieBilly <billyrobinson2001@outlook.com>
2020-05-07 23:03:28 +0100

    punish test

--- commands/punish.js
+++ commands/punish.js
@@ -12,10 +12,10 @@ module.exports = {
 	async execute(client, message, args) {
         
         const config = "not found"
-        if (args[1] === "global") {const config = punishmentconfig.punishments.global}
-        if (args[1] === "community") {const config = punishmentconfig.punishments.community}
-        if (args[1] === "official") {const config = punishmentconfig.punishments.official}
-        if (config === "not found") {message.reply("You haven't entered a correct punishment type")}
+        if (`${args[1]}` == "global") {const config = punishmentconfig.punishments.global}
+        if (`${args[1]}` == "community") {const config = punishmentconfig.punishments.community}
+        if (`${args[1]}` == "official") {const config = punishmentconfig.punishments.official}
+        if (`${args[1]}` == "not found") {message.reply("You haven't entered a correct punishment type")}
 
         for (const idk in config) {if (idk === args[2]) {message.reply(`**Found!** reason: ${idk} track: ${global[idk][0]} weight: ${global[idk][1]}`)}}
 
