1581beb15664fcb51591d68c6bd080146cf5386a
CookieBilly <41408185+CookieBilly@users.noreply.github.com>
2020-05-08 07:19:02 +0100

    Create slotHandler.js

---
+++ API/slotHandler.js
@@ -0,0 +1,46 @@
+const slot = {};
+
+slot.create = async (data = {}) => {
+
+    // validate
+    
+    // enter in db
+    
+    // announce on discord
+    
+    // pm hosts
+
+}
+
+slot.bid = async (uslot, ser, amount) => {
+
+    // validate user has enough
+    
+    // register new bid
+    
+    // push update to page (socketio)
+
+}
+
+slot.cancel = async (slot, user) => {
+
+    // validate request
+    
+    // remove from db
+
+    // announce on discord
+
+}
+
+slot.fetch = async (slot) => {
+
+    // fetch slot information
+
+    // return json
+}
+
+slot.list = async () => {
+
+    // return list of slots
+
+}
