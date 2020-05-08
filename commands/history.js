85c1257663a2807bd3f457f880655edf896c4860
CookieBilly <41408185+CookieBilly@users.noreply.github.com>
2020-05-09 00:33:28 +0100

    Create history.js

---
+++ commands/history.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+const punishmentconfig = require('../settings/punishments.json');
+const punishmenthandler = require('../API/punishmentHandler.js');
+const embeds = require('../modules/embeds.js');
+
+module.exports = {
+	name: 'history',
+	description: 'Fetch punishment history',
+	usage: 'history [userid]',
+	example: 'history 213849560508792832',
+	requiredRoles: ['🧍🏿‍♂️  Bouncer'],
+	allowedUsers: [],
+	argsNeeded: 1,
+	async execute(client, message, args) {
+        let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+        const punishEmbed = await new embeds.infoEmbed('working', message.channel, `Trying to find history of ${user}`, message.author, null).sendEmbed();
+        
+        const user = {
+            id: `${user.id}`
+        }
+
+        const punishmentstatus = await punishmenthandler.history(user)
+	    console.log(punishmentstatus)
+	
+        if (`${punishmentstatus.status}` == "success") {
+            // spit out good/bad error
+            return new embeds.infoEmbed('success', punishEmbed, `Punishment history:\n\`\`\`${punishmentstatus.result}\`\`\``, message.author, null).editEmbed();
+        } else {
+            return new embeds.infoEmbed('error', punishEmbed, `Failed to remove punishment\n\nReason: \`\`\`${punishmentstatus.reason}\`\`\``, message.author, null).editEmbed();
+        }
+
+
+
+  	},
+
+};
