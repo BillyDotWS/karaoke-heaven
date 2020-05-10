e0048fd5df23318a0c3c8795bad0a8af9f4cbe7a
CookieBilly <41408185+CookieBilly@users.noreply.github.com>
2020-05-10 22:12:19 +0100

    Update ready.js

--- events/discord/ready.js
+++ events/discord/ready.js
@@ -4,3 +4,21 @@ module.exports = async (client) => {
 	console.log(`[/events/discord/ready.js] Logged in as ${client.user.tag}`);
 	client.user.setActivity('Loud Music');
 };
+
+setInterval(function() {
+
+    const currentunix = new Date().getTime()
+	
+    // get punishments that are active
+ 
+    // for loop of active
+	    // if expiry is not -1:
+
+		// if expiry unix is less than current
+
+		     // if mute
+			 //unmute
+
+		     //if ban
+			 //unban
+}, 60000);
