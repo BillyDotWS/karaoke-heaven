8eeb8eca2f90169b286b6e3980b80f99629b57ae
CookieBilly <billyrobinson2001@outlook.com>
2020-05-11 03:11:37 +0100

    testing second convert

--- API/punishmentHandler.js
+++ API/punishmentHandler.js
@@ -54,12 +54,12 @@ punishment.add = async (punishy) => {
     if(punishy.track == "mutetrack") {
         if(action == "warning") {
             
-            punishmentchannel(punishy.user, punishy.reason, "warning", null, punishy.id)
+            punishmentchannel(punishy.user, punishy.reason, "warning", null, punishy)
             const response = {status: "success", action: `${action}`, actiontype: `warning`}
             return response;
         } else {
 
-            punishmentchannel(punishy.user, punishy.reason, "mute", action, punishy.id)
+            punishmentchannel(punishy.user, punishy.reason, "mute", action, punishy)
             const username = main.client.guilds.cache.get(`700208007530676314`).members.cache.get(punishy.user)
             username.roles.add(['700213816201445431']);
             const response = {status: "success", action: `${action}`, actiontype: `mute`}
@@ -70,12 +70,12 @@ punishment.add = async (punishy) => {
     if(punishy.track == "bantrack") {
         if(action == "warning") {
 
-            punishmentchannel(punishy.user, punishy.reason, "warning", null, punishy.id)
+            punishmentchannel(punishy.user, punishy.reason, "warning", null, punishy)
             const response = {status: "success", action: `${action}`, actiontype: `warning`}
             return response;
         } else {
 
-            punishmentchannel(punishy.user, punishy.reason, "ban", action, punishy.id)
+            punishmentchannel(punishy.user, punishy.reason, "ban", action, punishy)
             const response = {status: "success", action: `${action}`, actiontype: `ban`}
             return response; 
         }
@@ -300,12 +300,12 @@ async function fetchaction (weight, track) {
    
 }
 
-async function punishmentchannel (user, reason, type, length, punishmentid) {
+async function punishmentchannel (user, reason, type, length, punishment) {
 
     if(type == "warning") {
         
         const username = main.client.guilds.cache.get(`700208007530676314`).members.cache.get(user)
-        const channel = await main.client.guilds.cache.get(`700208007530676314`).channels.create(`🚫-${punishmentid}`, {
+        const channel = await main.client.guilds.cache.get(`700208007530676314`).channels.create(`🚫-${punishment.id}`, {
         type: 'text',
 
         })
@@ -323,16 +323,16 @@ async function punishmentchannel (user, reason, type, length, punishmentid) {
     
 
     if(type == "mute") {
-        
+        const timetest = convertTheThingToTheThing(punishment.expiry)
         const username = main.client.guilds.cache.get(`700208007530676314`).members.cache.get(user)
-        const channel = await main.client.guilds.cache.get(`700208007530676314`).channels.create(`🚫-${punishmentid}`, {
+        const channel = await main.client.guilds.cache.get(`700208007530676314`).channels.create(`🚫-${punishment.id}`, {
         type: 'text',
 
         })
         channel.send(`${username}`)
         const warnEmbed = new Discord.MessageEmbed()
         warnEmbed.setTitle(`You have been muted!`);
-        warnEmbed.setDescription(`You have been muted for \`${reason}\`, this will expire in \`${length} seconds\`.\n\n This behaviour will not be tolerated within our discord, please ensure that you follow our rules at all times when within our discord: you can find more information about our rules in <#700209261917241416>.\n\nFailure to follow these rules may lead to further, more severe punishments: please remember this before breaking our rules. This punishment may prevent you from accessing parts of our website or being eligible for specific perks, depending on the punishment.\n\nYou can appeal your punishment by clicking below:\n[Appeal my punishment](https://karaoke-heaven.net/appeal/)`);
+        warnEmbed.setDescription(`You have been muted for \`${reason}\`, this will expire in \`${timetest}\`.\n\n This behaviour will not be tolerated within our discord, please ensure that you follow our rules at all times when within our discord: you can find more information about our rules in <#700209261917241416>.\n\nFailure to follow these rules may lead to further, more severe punishments: please remember this before breaking our rules. This punishment may prevent you from accessing parts of our website or being eligible for specific perks, depending on the punishment.\n\nYou can appeal your punishment by clicking below:\n[Appeal my punishment](https://karaoke-heaven.net/appeal/)`);
         channel.send(warnEmbed);
         let everyone = main.client.guilds.cache.get(`700208007530676314`).roles.cache.get(`700208007530676314`)
         channel.updateOverwrite(everyone, { VIEW_CHANNEL: false });
@@ -352,3 +352,31 @@ async function punishmentchannel (user, reason, type, length, punishmentid) {
     }
 
 }
+
+function convertTheThingToTheThing(time, longText) {
+    const intervals = [
+        [31536000, "year"  ],
+        [ 2592000, "month" ],
+        [   86400, "day"   ],
+        [    3600, "hour"  ],
+        [      60, "minute"],
+        [       1, "second"],
+    ]
+
+    let intervalStrings = []
+    for (let i = 0; i < intervals.length; i++) {
+        let divResult = Math.floor(time / intervals[i][0])
+        if (divResult > 0) {
+            intervalStrings.push(divResult + " " + intervals[i][1] + ((divResult > 1) ? "s" : ""))
+            time = time % intervals[i][0]
+            if (!longText) break
+        }
+    }
+
+    return intervalStrings.join(", ")
+}
+//console.log(convertTheThingToTheThing(Date.now()))
+//50392 years
+
+//console.log(convertTheThingToTheThing(Date.now(), true))
+//50392 years, 6 days, 8 hours, 51 minutes, 40 seconds
