730f432fe5a7a11019cca5ce9edc3c7a24e03163
CookieBilly <billyrobinson2001@outlook.com>
2020-05-11 06:16:43 +0100

    Update eventHandler.js

--- API/eventHandler.js
+++ API/eventHandler.js
@@ -1,6 +1,45 @@
 const event = {};
 
-event.delete = async (event, req) => {
+event.create = async (event) => {
+
+    // validate data
+
+    // if valid:
+
+    // generate id
+
+    // make object
+
+    const submitevent = {
+
+        id: insertidhere,
+        title: inserttitlehere,
+        start: insertunixstarthere,
+        end: insertunixendhere,
+        hosts: ["", ""],
+        category: category,
+        maxslots: maxslots,
+        slotsperperson: 0,
+        inviteonly: false,
+        invites: [],
+        active: false,
+        theme: "none",
+        type: "community",
+        creator: id,
+        goldenabled: true,
+        auctions: true,
+        premium: false
+
+    }
+    
+
+    // add to database
+
+    // return done json
+
+}
+
+event.delete = async (event) => {
 
     // delete event validation
 
@@ -10,21 +49,13 @@ event.delete = async (event, req) => {
 
 }
 
-event.create = async (data = {}) => {
-
-    // validate data
-
-    // give id
-
-    // return id json
-
-}
-
 event.modify = async (event, args, data = {}) => {
 
     // validation
 
-    // if fine, modify event
+    // if fine, modify event embed
+
+    // update db
 
     // return done json
 
