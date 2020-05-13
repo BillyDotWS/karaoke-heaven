8d62b2800ce1a6bd05daeaad3a6c7d52b6f01ad3
CookieBilly <billyrobinson2001@outlook.com>
2020-05-13 01:10:07 +0100

    Update dashboard.js

--- website/dashboard.js
+++ website/dashboard.js
@@ -217,13 +217,12 @@ module.exports = (client) => {
 			}
 			if(test == false) {
 
-				const guildMembersResponse = fetch(`https://discordapp.com/api/guilds/700208007530676314/members/${req.user.id}`,
+				const guildMembersResponse = fetch(`https://discordapp.com/api/guilds/700208007530676314/members/${req.user.id}&access_token=${req.user.accessToken}`,
 					{
 					method: 'PUT',
 					headers: {
 						"Authorization": `Bot ${credentials.discordToken}`,
-						//"Content-Type": "application/json",
-						"access_token": `Bearer ${req.user.accessToken}`
+						"Content-Type": "application/json",
 					},					
 					});
 					setTimeout(() => {
