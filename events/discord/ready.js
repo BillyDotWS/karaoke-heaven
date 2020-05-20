d53e13b3a5ba64b9c57de4fe1d15bc87d7709930
CookieBilly <41408185+CookieBilly@users.noreply.github.com>
2020-05-20 09:32:05 +0100

    Update ready.js

--- events/discord/ready.js
+++ events/discord/ready.js
@@ -123,7 +123,7 @@ setInterval(async function() {
 					console.log(`------------------------`)
 					console.log(eventinfo[0].embedid)
 
-					const shrugUsers = await main.client.guilds.cache.get(`700208007530676314`).channels.cache.get(`700209759080546345`).fetch(eventinfo[0].embedid).reactions.cache.get(`711061304240242738`).users.cache.map(users => users.id);
+					const shrugUsers = await main.client.guilds.cache.get(`700208007530676314`).channels.cache.get(`700209759080546345`).fetch(eventinfo[0].embedid).reactions.cache.get(`711061304240242738`)//.users.cache.map(users => users.id);
 					console.log(`------------------------`)
 					console.log(shrugUsers)
 					  
