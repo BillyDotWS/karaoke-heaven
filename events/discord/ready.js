b04b64706cc39f0e2d043e6f1bc643020c84fd07
CookieBilly <41408185+CookieBilly@users.noreply.github.com>
2020-05-20 10:46:56 +0100

    Update ready.js

--- events/discord/ready.js
+++ events/discord/ready.js
@@ -123,7 +123,8 @@ setInterval(async function() {
 					console.log(`------------------------`)
 					const embedidentify = toString(eventinfo[0].embedid)
 
-					await main.client.guilds.cache.get("700208007530676314").channels.cache.get("700209759080546345").messages.cache.fetch()
+					
+					await main.client.channels.cache.get('700209759080546345').messages.fetch(embedidentify);
 					const shrugUsers = main.client.guilds.cache.get("700208007530676314").channels.cache.get("700209759080546345").messages.cache.get(embedidentify).reactions.cache.get(`711061304240242738`).users.cache.map(users => users.id)
 					console.log(`------------------------`)
 					console.log(shrugUsers)
