25fe6337ee1b95d5038a7b918d319073a10f1cce
CookieBilly <41408185+CookieBilly@users.noreply.github.com>
2020-05-21 07:20:17 +0100

    Update forms.js

--- templates/public/scripts/forms.js
+++ templates/public/scripts/forms.js
@@ -1,27 +1,27 @@
 function testclone() {
-  swal.mixin({
+  Swal.mixin({
+    input: 'text',
     confirmButtonText: 'Next &rarr;',
     showCancelButton: true,
     progressSteps: ['1', '2', '3']
-    }).queue([
-      {
-        title: 'Question 1',
-        text: 'Chaining swal2 modals is easy',
-        input: 'text'
-      },
-      'Question 2',
-      'Question 3'
-    ]).then((result) => {
+  }).queue([
+    {
+      title: 'Question 1',
+      text: 'Chaining swal2 modals is easy'
+    },
+    'Question 2',
+    'Question 3'
+  ]).then((result) => {
     if (result.value) {
       const answers = JSON.stringify(result.value)
       Swal.fire({
         title: 'All done!',
-          html: `
-            Your answers:
-            <pre><code>${answers}</code></pre>
-      `   ,
+        html: `
+          Your answers:
+          <pre><code>${answers}</code></pre>
+        `,
         confirmButtonText: 'Lovely!'
       })
-        }
     }
-    )}
+  })
+}
