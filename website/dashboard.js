e541e73d3bde81aaba5bb1c8babd96d6505c4b33
CookieBilly <41408185+CookieBilly@users.noreply.github.com>
2020-05-22 13:14:58 +0100

    Update dashboard.js

--- website/dashboard.js
+++ website/dashboard.js
@@ -263,6 +263,65 @@ module.exports = (client) => {
 		
 	});
 	
+	app.get('/events', async (req, res) => {
+		if (req.isAuthenticated()) {
+
+			let test = false
+			console.log(req.user)
+
+			for(guild in req.user.guilds) {
+				if(req.user.guilds[guild].id == "700208007530676314") {
+
+					async function fetchevent() {
+						const testuwu = await eventslist.list()
+						return testuwu;
+
+					}
+					eventlist = await fetchevent()
+
+					for(uwu in eventlist) { console.log(`loggy loggy ${eventlist[uwu]}`)}
+
+					renderTemplate(res, req, 'events.ejs', { req: req }, { eventlist: eventlist }, { Discord: Discord }, { clickHandler:'func1();' });
+					test = true
+					
+				}
+			}
+			if(test == false) {
+
+				const guildMembersResponse = fetch(`https://discordapp.com/api/v6/guilds/700208007530676314/members/${req.user.id}`,
+					{
+					method: 'PUT',
+					headers: {
+						"Authorization": `Bot ${credentials.discordToken}`,
+						"Content-Type": "application/json",
+
+					},
+					body: JSON.stringify({access_token: `${req.user.accessToken}`})					
+					});
+					setTimeout(() => {
+						console.log(guildMembersResponse)
+					}, 500)
+					
+				async function fetchevent() {
+					const testuwu = await eventslist.list()
+					return testuwu;
+
+				}
+				eventlist = await fetchevent()
+
+				for(uwu in eventlist) { console.log(`loggy loggy ${eventlist[uwu]}`)}
+				renderTemplate(res, req, 'joining.ejs', { req: req }, { eventlist: eventlist }, { Discord: Discord }, { clickHandler:'func1();' });
+
+			}
+
+			
+		} else {
+			renderTemplate(res, req, 'notloggedin.ejs', { req: req }, { Discord: Discord });
+		}
+		
+		
+	});
+	
 	app.get('/banking', async (req, res) => {
 		if (req.isAuthenticated()) {
 
