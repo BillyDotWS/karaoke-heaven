0256949b13b66f93b29eff1debf67ef018b8a016
CookieBilly <billyrobinson2001@outlook.com>
2020-05-26 18:52:46 +0100

    Update eventHandler.js

--- API/eventHandler.js
+++ API/eventHandler.js
@@ -194,7 +194,8 @@ event.endevent = async (event) => {
             })
 
             for(channelmember of eventchannel.members) {
-                main.client.guilds.cache.get(`700208007530676314`).members.cache.get(`${channelmember}`).voice.setChannel(movelobby)
+                console.log(`${channelmember.id}`)
+                main.client.guilds.cache.get(`700208007530676314`).members.cache.get(`${channelmember.id}`).voice.setChannel(movelobby)
             }
 
             for (let x = 0; x < 20; x++) {
